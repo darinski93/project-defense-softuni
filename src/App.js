211e1fd10e4677587176826b8a810909932a6c4c
Darios Petrov <darios.petrov93@gmail.com>
2023-04-18 20:09:04 +0300

    add Error component

--- src/App.js
+++ src/App.js
@@ -4,23 +4,18 @@ import { ProductProvider } from './contexts/ProductContext';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Home from './components/Home/Home';
-
 import { Routes, Route } from 'react-router-dom'
 import CreatePizza from './components/Create/Create';
 import DetailsPizza from './components/Details/Details';
-
-
-
 import Menu from './components/Menu/Menu';
 import Login from './components/Login/Login';
 import Register from './components/Register/Register';
-
 import EditPizza from './components/Edit/Edit';
-
 import { AuthProvider } from './contexts/AuthContext';
 import Logout from './components/Logout/Logout';
 import { RouteGuard } from './components/common/RouteGuard';
 import { ProductOwner } from './components/common/ProductOwner';
+import Error from './components/Error/Error';
 
 
 
@@ -59,6 +54,7 @@ function App() {
               <Route path='/logout' element={<Logout />} />
             </Route>
 
+            <Route path='/*' element={<Error />} />
           </Routes>
         </main>
         <Footer />
---
+++ src/components/Error/Error.js
@@ -0,0 +1,7 @@
+export default function Error() {
+    return (
+        <span>
+           <h3>Error 404 &#128533; </h3>
+        </span>
+    )
+}
